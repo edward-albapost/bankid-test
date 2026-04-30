e91692ea0a023284b21c60eca5ff821bdf4e14a2
edward-albapost <edward@albapost.se>
2026-04-30 16:20:23 +0200

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import Image from "next/image";
+import { useCriiptoVerify } from "@criipto/verify-react";
 
 export default function Home() {
+  const { claims, isLoading, logout, error } = useCriiptoVerify();
+
+  console.log(claims);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <a href="/login">Log in :)</a>
