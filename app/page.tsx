bcb3826df882dbacac99a9afcdca5324a647cbd1
edward-albapost <edward@albapost.se>
2026-04-30 16:40:24 +0200

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,11 @@ import { useCriiptoVerify } from "@criipto/verify-react";
 export default function Home() {
   const { claims, isLoading, logout, error } = useCriiptoVerify();
 
-
   const handleLogout = () => {
     logout({ redirectUri: 'https://bankid-test-blond.vercel.app/' });
   };
+
+  console.log(claims);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
