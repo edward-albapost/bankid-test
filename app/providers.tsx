233ab7b28a98e0b819848a851868bbdb66946ddb
edward-albapost <edward@albapost.se>
2026-04-30 15:49:36 +0200

    lol getting tired

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,18 +2,15 @@
 
 import { CriiptoVerifyProvider } from '@criipto/verify-react';
 
-
-
 export function IduraProvider({ children }: { children: React.ReactNode }) {
   return (
     <CriiptoVerifyProvider
-      domain="samples.criipto.id";
-      clientID="urn:criipto:samples:criipto:verify-react";
-      redirectUri="http://localhost:3000";
+      domain="samples.criipto.id"
+      clientID="urn:criipto:samples:criipto:verify-react"
+      redirectUri="http://localhost:3000"
       sessionStore={null}
       prompt="login"
       checkSession={false}
-      // language="sv"
     >
       {children}
     </CriiptoVerifyProvider>
