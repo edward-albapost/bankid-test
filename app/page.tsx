948b1ff6d0696374cb6fbe2e2a185c2038b01b22
edward-albapost <edward@albapost.se>
2026-04-30 16:30:05 +0200

    added name for checking

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,14 @@ import { useCriiptoVerify } from "@criipto/verify-react";
 export default function Home() {
   const { claims, isLoading, logout, error } = useCriiptoVerify();
 
+  
+
   console.log(claims);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <a href="/login">Log in :)</a>
+      {!claims ? <p>no claims</p> : <p>{claims?.givenname}</p>}
     </div>
   );
 }
